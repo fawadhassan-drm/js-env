526d53ffa21567888418da5e67c4058864a1334c
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-17 17:55:42 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 123
+// accountId = 345
+console.log(accountId);
+
+let accountName = "Fawad"
+accountName = "My Name has been change"
+console.log(accountName)
+console.log(typeof accountName)
+
+let dummy = Number(accountName)
+console.log(typeof dummy)
+console.log(typeof accountName)
