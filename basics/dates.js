e3eed5de1f27f651f0f9a3a785c8e37e4327b721
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-18 12:50:24 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,9 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getFullYear());
+// console.log(myDate.getHours());
+
+let a = Date.now();
+console.log(a.toLocaleString());
+console.log(a.toString());
+
