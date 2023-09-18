4ee5924c1fdd48b6a3143be35e672659173e5324
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-18 12:08:21 +0000

    number and maths

---
+++ basics/nums-math.js
@@ -0,0 +1,16 @@
+const score = 100
+console.log(score);
+console.log(typeof score);
+
+const balance = new Number(400)
+console.log(balance);
+console.log(typeof balance);
+
+
+console.log(balance.toString());
+console.log(typeof balance);
+
+console.log(Math)
+console.log(Math.random())
+console.log(Math.random()+100)
+console.log(Math.random()* 10)
