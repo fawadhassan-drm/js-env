b9f327c7b058d27c15e819bc22ed8d8cd4f40f08
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-18 10:39:50 +0000

    srings

---
+++ basics/strings.js
@@ -0,0 +1,33 @@
+let a = "Fawad"
+let b = "Hassan"
+
+// console.log("My first name is " + a + " and last name is " + b);
+
+// console.log(`My first name is ${a} and last name is ${b}`);
+
+// console.log(a.toLowerCase());
+// console.log(a.charAt(2));
+// console.log(a.indexOf('d'));
+// console.log(a.includes('w'));
+// console.log(a.includes('h'));
+// console.log(a.replace('f', 'j'));
+
+
+
+const str1 = "My Job is web development"
+
+console.log(str1.replace('web', 'app'));
+console.log(str1);
+
+const str2 = "My name is Fawad Hassan"
+const str3 = str2
+
+console.log(str3.length);
+
+const word1 = "          your         hobby     "
+const word2 = "Your hobby"
+
+console.log(word1);
+console.log(word2.trim());
+console.log(word2.trimStart());
+console.log(word2.trimEnd());
