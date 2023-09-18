285a7cd91d4cfca3985fb6b244a4deea38ee116d
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-18 06:09:12 +0000

    02_datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,28 @@
+// Data Types
+
+// nnbbssu
+
+// Number
+// null
+// Boolean
+// BigInt
+// String
+// Symbol
+// undefined
+
+let num = 123;
+// console.log(num)
+// console.log(typeof num)
+// console.log(typeof 123)
+
+let a = null;
+// console.log(typeof null);
+
+let result = true;
+// console.log(typeof result);
+
+let b = "Dot republic media"
+// console.log(typeof b);
+
+let c = undefined
+console.log(typeof c);
