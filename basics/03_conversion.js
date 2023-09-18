5491c6fdce57fd3d28717ce23130bfa2c229a3d5
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-18 06:16:40 +0000

    03_conversion

---
+++ basics/03_conversion.js
@@ -0,0 +1,15 @@
+let score = "33"
+// console.log(score);
+// console.log(typeof score);
+
+let valScore = Number(score)
+// console.log(typeof valScore);
+// console.log(valScore);
+
+console.log("1" + 2)
+console.log(1 + "2");
+
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
+
+
