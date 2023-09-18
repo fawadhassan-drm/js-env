5dade275cb0569669f41b4cfdf9ecdd4b521bbe3
fawadhassan-drm <145224837+fawadhassan-drm@users.noreply.github.com>
2023-09-18 06:02:01 +0000

    01_variables

---
+++ basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1122
+let accountName = "Fawad"
+var accountYear = 1998
+age = 25
+
+// accountId = 2211 // Not allowed
+accountName = "Hassan"
+accountYear = 2002
+age = 23
+
+
+console.log(accountId);
+console.table([accountId, accountName, accountYear, age])
